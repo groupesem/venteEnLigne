f8b812bcaab603023210840c6a76c6a468501f77
sanaOueslati <trichisana22@gmail.com>
2017-12-22 13:56:13 +0100

    Xml de Se connecter

---
+++ app/src/main/java/com/example/takwa/venteelectronique/SeConnecterActivity.java
@@ -0,0 +1,13 @@
+package com.example.takwa.venteelectronique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeConnecterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.se_connecter);
+    }
+}
