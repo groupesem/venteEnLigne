a98d1d44cfc2040392b8ca79c56f4ca8234b2a50
takwa-abdelkader <abdelkader.takwa@gmail.com>
2017-12-22 13:53:53 +0100

    Sign in commit (xml)

---
+++ app/src/main/java/com/example/takwa/venteelectronique/Sign_in.java
@@ -0,0 +1,14 @@
+package com.example.takwa.venteelectronique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+}
