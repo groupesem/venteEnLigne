f11a1820e3cf1a43e8fd4bbe6207c161b312e99a
sanakthiri <sanakthiri8@gmail.com>
2017-12-22 12:37:54 +0100

    test branche commit

--- app/src/main/java/com/example/takwa/venteelectronique/MainActivity.java
+++ app/src/main/java/com/example/takwa/venteelectronique/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maint);
+        android.widget.Toast.makeText(this, "tes", Toast.LENGTH_SHORT).show();
     }
 }
