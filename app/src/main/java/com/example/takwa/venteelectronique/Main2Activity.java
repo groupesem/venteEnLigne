e02cc47d82e16481f95f2787f8d06b4d48dc7e98
ESLEMM <islemkhemiri09@gmail.com>
2018-01-06 19:31:37 +0000

    test eslem

---
+++ app/src/main/java/com/example/takwa/venteelectronique/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.takwa.venteelectronique;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b_signin = (Button) findViewById(R.id.b_signin);
+
+    }
+
+    public void signin(View view)
+    {
+        Intent myIntent = new Intent(getApplicationContext(), Sign_in.class);
+        startActivity(myIntent);
+    }
+}
