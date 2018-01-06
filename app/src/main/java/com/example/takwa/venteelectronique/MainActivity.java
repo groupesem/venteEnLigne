a9b0861c5db813cade0121ff3dffa933bc7394c0
sanakthiri <sanakthiri8@gmail.com>
2018-01-06 13:59:53 +0100

    hedha la page d'acceui

--- app/src/main/java/com/example/takwa/venteelectronique/MainActivity.java
+++ app/src/main/java/com/example/takwa/venteelectronique/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.takwa.venteelectronique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maint);
-        android.widget.Toast.makeText(this, "tes", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+
+        
     }
 }
