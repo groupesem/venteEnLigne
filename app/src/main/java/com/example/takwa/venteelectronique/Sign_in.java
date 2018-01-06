b44467bd4e332101775a490042e269105656306d
takwa-abdelkader <abdelkader.takwa@gmail.com>
2018-01-06 12:42:55 +0100

    Sign in commit (xml + java ) client (java )

--- app/src/main/java/com/example/takwa/venteelectronique/Sign_in.java
+++ app/src/main/java/com/example/takwa/venteelectronique/Sign_in.java
@@ -2,6 +2,8 @@ package com.example.takwa.venteelectronique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Sign_in extends AppCompatActivity {
 
@@ -9,6 +11,18 @@ public class Sign_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
+    }
+
+    public void incrire(View view)
+    {
+        EditText nom = (EditText)findViewById(R.id.tv_name);
+        EditText prenom = (EditText)findViewById(R.id.tv_prenom);
+        EditText  tel = (EditText)findViewById(R.id.tv_tel);
+        EditText  pm = (EditText)findViewById(R.id.tv_pm);
+        EditText adresse = (EditText)findViewById(R.id.tv_adresse);
+
 
     }
 }
